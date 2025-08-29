b04f8bc47208bf53f361909da0964cb29fa2680a
Your Name <your@email.com>
2025-08-29 17:42:55 +0530

    first commit

--- function.js
+++ function.js
@@ -33,10 +33,3 @@ hoverBox.onmouseout = function() {
 };
 
 
-echo "# Eventlistners" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/sharmamayankwins-create/Eventlistners.git
-git push -u origin main
